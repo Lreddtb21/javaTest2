75f67174134dc09c480c53a46ad04b51f94857a7
Lreddtb21 <lovettaredd@gmail.com>
2021-05-05 20:18:36 -0400

    branch

---
+++ QuizJava/BuildingProgram (1).java
@@ -0,0 +1,54 @@
+package student;
+
+import java.util.Scanner; //import the Scanner
+
+
+
+public class BuildingProgram {
+	/*
+	 * What was the highest score? Bonus: Who was it?
+What was the lowest score? Bonus: Who was it?
+What was the average score for the class?
+How many students got an A (90+)?
+How many students got a B (80 - 89)?
+How many students got a C (70 - 79)?
+How many students failed (69 or lower)
+	 */
+
+	public static void main(String[] args) {
+		
+			String name;
+			String stu_id;
+			int score
+			public Studnet () {
+				this("", "", 0);
+				
+			}
+			public Student(String initNmae, Stringi= initId, InitScore)
+				
+		
+			
+			
+	
+		
+	
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		}
+		
+		
+		
+		
+		
+
+
+		
+
+
+
+	}
+
+}
