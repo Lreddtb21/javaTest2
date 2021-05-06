2e5cb8cb5d3a2a22288296a8c25ac037fd1cfbe6
imreed3869 <82849126+imreed3869@users.noreply.github.com>
2021-05-05 20:28:07 -0400

    Add files via upload

---
+++ QuizJava/TestScores.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+import Calculate.calculate;
+
+public class TestScores {
+
+	public static void main(String[] args) {
+		
+		ArrayList <String> StudentNames = new ArrayList <String> ();
+		private char []StudentLetterGrades = { "A", "B", "C", "F" };
+		double [ ][ ] studentTestScores ;
+		"calculate" AvergageTestScores (double [] TestScores)		
+		for( int StudentTestScore =0; [] StudentTestScore <studentTestScore ++> *;)
+			
+				
+
+	}
+
+}
